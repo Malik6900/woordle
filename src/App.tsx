bd3e52f03a48caba030b38bf44193772da6ab0eb
malik6900 <alikarriem786@gmail.com>
2022-07-03 22:39:35 +0200

    added padding

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ export default function App() {
   rows = rows.concat(Array(guessesRemaining).fill(''));
 
   return (
-    <div className="mx-auto w-96 relative h-screen">
+    <div className="mx-auto w-96 relative h-screen p-2">
       <header className="border-b border-gray-400 py-4">
         <h1 className="text-3xl font-bold text-center uppercase">
           <span className="text-[#bada55]">W</span>
