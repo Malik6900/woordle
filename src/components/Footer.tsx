48c205850724cf00832d2ff81a33615b33d44f13
malik6900 <alikarriem786@gmail.com>
2022-07-03 21:54:22 +0200

    added email contact

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,9 @@ export default function Footer() {
           <i className="fa-brands fa-facebook-f p-1"></i></a>| 
           <a href="https://twitter.com/intent/tweet" target="_blank"><i className="fa-brands fa-twitter p-1"></i></a>| 
           <a href="https://www.instagram.com/?hl=en" target="_blank"><i className="fa-brands fa-instagram-square p-1"></i></a>|
-          <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://woordle-afr.netlify.app" target="_blank"><i className="fa-brands fa-linkedin-in p-1"></i></a></div>
-        <div className="text-sm text-center bg-slate-200 p-2"><i className="fa-brands fa-react"></i> {new Date().getFullYear()}</div>
+          <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://woordle-afr.netlify.app" target="_blank"><i className="fa-brands fa-linkedin-in p-1"></i></a>
+          </div>
+        <div className="text-sm text-center bg-slate-200 p-2"><a href="mailto:milesmedia9@gmail.com">Contact</a> <i className="fa-brands fa-react"></i> {new Date().getFullYear()}</div>
     </div>
   )
 }
